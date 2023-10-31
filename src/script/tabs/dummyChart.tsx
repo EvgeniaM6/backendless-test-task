81d640001fa4b59e7fb0b10754e51bc086d9ff85
EvgeniaM6 <evgminukova6@gmail.com>
2023-10-31 18:04:51 +0200

    feat: add tabs json and tab components

---
+++ src/script/tabs/dummyChart.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const DummyChart = () => {
+  return (
+    <div
+      style={{
+        borderRadius: '50%',
+        backgroundColor: 'coral',
+        width: '100px',
+        height: '100px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      Dummy Chart
+    </div>
+  );
+};
+
+export default DummyChart;
---
+++ src/script/tabs/dummyList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DummyList = () => {
+  return (
+    <ul>
+      <li>Dummy</li>
+      <li>List</li>
+    </ul>
+  );
+};
+
+export default DummyList;
