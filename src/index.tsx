43a2bc18af0480202384b877099ac5bc7b5c4c0e
EvgeniaM6 <evgminukova6@gmail.com>
2023-11-03 15:43:24 +0200

    fix: change router for gh-pages deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './script/App';
 import './sass/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
